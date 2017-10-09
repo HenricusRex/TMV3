fc327d43a041a00d1df2fb1789d51c6c646aac57
HenricusRex <heinz.schueller@web.de>
2017-10-09 14:57:52 +0200

    22.8

---
+++ DB/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='TMV3',
+    version='',
+    packages=[''],
+    package_dir={'': 'DB'},
+    url='',
+    license='',
+    author='Heinz',
+    author_email='',
+    description=''
+)
